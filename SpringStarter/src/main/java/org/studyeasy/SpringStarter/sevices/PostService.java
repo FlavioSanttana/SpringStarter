98546153739cec348d48fccb4c346edd5d8aa863
Flávio Santana <f.o.santana@gmail.com>
2026-01-26 13:59:49 -0300

    Implementado repository and services class

---
+++ SpringStarter/src/main/java/org/studyeasy/SpringStarter/sevices/PostService.java
@@ -0,0 +1,38 @@
+package org.studyeasy.SpringStarter.sevices;
+
+import java.nio.file.OpenOption;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.studyeasy.SpringStarter.models.Post;
+import org.studyeasy.SpringStarter.repositories.PostRepository;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Optional<Post> getById(Long id){
+        return postRepository.findById(id);
+    }
+    
+    public List<Post> getAll(Long id){
+        return postRepository.findAll();
+    }
+
+    public void delete (Post post){
+        postRepository.delete(post);
+    }
+
+    public Post save(Post post){
+        if(post.getId()==null){
+            post.setCreatedAt(LocalDateTime.now());;
+        }
+        return postRepository.save(post);
+    }
+    
+}
