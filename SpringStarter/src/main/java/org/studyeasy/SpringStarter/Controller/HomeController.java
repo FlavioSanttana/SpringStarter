51478290d9a029d75dd0590aaca560ddf74acbec
Flávio Santana <f.o.santana@gmail.com>
2026-01-27 21:33:27 -0300

    Implementado rotina para apresentar dados na home.html

--- SpringStarter/src/main/java/org/studyeasy/SpringStarter/Controller/HomeController.java
+++ SpringStarter/src/main/java/org/studyeasy/SpringStarter/Controller/HomeController.java
@@ -1,15 +1,32 @@
 package org.studyeasy.SpringStarter.Controller;
 
+import java.util.List;
+
+//import org.hibernate.mapping.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.studyeasy.SpringStarter.models.Post;
 //import org.springframework.web.bind.annotation.RequestParam;
+import org.studyeasy.SpringStarter.sevices.PostService;
+
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private PostService postservice; 
+
     @GetMapping("/")    
     public String home (Model model){
+        
+        //List<Post> posts = postservice.getAll();
+        List<Post> posts = postservice.getAll();
+        
+        model.addAttribute("posts", posts);        
+        //String attributeName;
+        //model.addAttribute("posts", posts);
         return "Home";
     }
     
