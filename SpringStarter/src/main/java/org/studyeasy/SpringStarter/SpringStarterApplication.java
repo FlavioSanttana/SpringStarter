6a5a5957f0c6f545161a900eb2ea3026f218a2d6
Flávio Santana <f.o.santana@gmail.com>
2026-02-13 20:28:04 -0300

    Implementado formulário register

--- SpringStarter/src/main/java/org/studyeasy/SpringStarter/SpringStarterApplication.java
+++ SpringStarter/src/main/java/org/studyeasy/SpringStarter/SpringStarterApplication.java
@@ -1,13 +1,30 @@
-	package org.studyeasy.SpringStarter;
+package org.studyeasy.SpringStarter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.studyeasy.SpringStarter.models.Account;
+import org.studyeasy.SpringStarter.sevices.AccountService;
 
 @SpringBootApplication
 public class SpringStarterApplication {
 
+	@Autowired
+	private AccountService accountService; 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringStarterApplication.class, args);
+	}
+
+	@PostMapping("/register")
+	public String register(@ModelAttribute Account account){
+
+		accountService.save(account);
+
+		return "redirect:/";
+
 	}
 
 }
